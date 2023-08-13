3ea454754661f1377b1724092b567fc493d7549b
linarcx <linarcx@gmail.com>
2023-08-13 14:46:43 +0200

    updated

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
+<h1 align="center">
+	<img width="500" src="assets/matrix.png" alt="Trinity">
+	<br>
+</h1>
+
 # Trinity
+Generate link network image from text files.
+
+# Idea
+I developed trinity since i need a tool represent all the links between my files. (i use these linked notes to make a second brain.)
+
+The idea is simple. i have bunch of simple `.txt` files in a flat hierarchy structure that can refer to each other.(like in **docs** directory)
+
+to create a graph of links:
+`./trinity.sh docs/matrix.txt > /dev/null  2>&1`
+
+it will create two files in output directory:
+1. matrix.gv
+2. matrix.png
+
+actually i converted all links that i find inside input file to [dot language](https://en.wikipedia.org/wiki/DOT_(graph_description_language)) and then create an image from it.
+
+this is the content of auto-generated `.gv` file:
+```
+graph matrix {
+  layout=sfdp;
+  edge [penwidth=5 color="#FFCC80"]
+  node [style="filled" penwidth=0 fillcolor="#D7CCC8" fontcolor="#424242"]
+  matrix [fontsize=30]
+
+  node [fontsize=25]
+  matrix -- {
+    neo
+    mouse
+    cypher
+    trinity
+    morpheus
+    boy
+    oracle
+    smith
+    merovingian
+    architect
+  }
+
+  graph [ranksep=2];
+}
+```
+
+now you can open the image:
+`xdg-open output/matrix.png`
+
+## dependencies
+- [graphviz](https://graphviz.org/)
+
+## Who is Trinity
+<h1 align="center">
+	<img width="500" src="assets/trinity.gif" alt="Trinity">
+	<br>
+</h1>
 
 ## License
 ![License](https://img.shields.io/github/license/LinArcX/trinity.svg?style=flat-square)
--- trinity.sh
+++ trinity.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #------------------------------------------------
+if [ -d output ]; then rm -r output; fi
+
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <input_file>"
   exit 1
@@ -13,10 +15,13 @@ fi
 
 #------------------------------------------------
 level=1
-font_size=28
+font_size=30
 input_text=$(cat "$input_file")
 base_name=$(basename "$input_file" .txt)
-gv_file="$base_name.gv"
+
+mkdir -p output
+
+gv_file="output/$base_name.gv"
 
 #------------------------------------------------
 print_inner_nodes() {
@@ -24,7 +29,7 @@ print_inner_nodes() {
   txt_words=($(grep -o '\b[a-zA-Z0-9_]*\.txt\b' "$input_file" | sed 's/\.txt$//'))
   array_size=${#txt_words[@]}
   ((level++))
-  ((font_size -= 4))
+  ((font_size -= 5))
 
   echo -e "\n  node [fontsize=$font_size]" >> $gv_file
   echo "  $base_name -- {" >> $gv_file
@@ -36,7 +41,7 @@ print_inner_nodes() {
 
 #-----------------------------------------------------------
 echo "graph $base_name {" >> $gv_file
-echo "  layout=twopi;" >> $gv_file
+echo "  layout=sfdp;" >> $gv_file
 echo "  edge [penwidth=5 color=\"#FFCC80\"]" >> $gv_file
 echo "  node [style=\"filled\" penwidth=0 fillcolor=\"#D7CCC8\" fontcolor=\"#424242\"]" >> $gv_file
 
@@ -48,11 +53,12 @@ echo -e "\n  graph [ranksep=$level];" >> $gv_file
 echo "}" >> $gv_file
 
 #----------------------------------------------------------
+
 # generate a .png from .gv file
-dot -Tpng $base_name.gv -o $base_name.png
+dot -Tpng $gv_file -o output/$base_name.png
 
 # remove .gv file
-rm $gv_file
+#rm $gv_file
 
 # view it with default image viewer
-xdg_open $base_name.png
+xdg-open output/$base_name.png
